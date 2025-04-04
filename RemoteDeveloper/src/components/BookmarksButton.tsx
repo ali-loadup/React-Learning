fbd62b454a271c730ed04c5bf638e5c4f6f453ed
Ali <ali@loadup-talent.de>
2025-04-04 13:57:43 +0200

    use useOnClickOutside hook to close popover

--- RemoteDeveloper/src/components/BookmarksButton.tsx
+++ RemoteDeveloper/src/components/BookmarksButton.tsx
@@ -1,28 +1,16 @@
 import { TriangleDownIcon } from "@radix-ui/react-icons";
 import { BookmarksPopover } from "./BookmarksPopover";
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
+import { useOnClickOutside } from "usehooks-ts";
 
 export default function BookmarksButton() {
   const [isOpen, setIsOpen] = useState(false);
   const buttonRef = useRef<HTMLButtonElement>(null);
   const popoverRef = useRef<HTMLDivElement>(null);
 
-  const handleClickOutside = (event: MouseEvent) => {
-    if (
-      event.target instanceof HTMLElement &&
-      !buttonRef.current?.contains(event.target) &&
-      !popoverRef.current?.contains(event.target)
-    ) {
-      setIsOpen(false);
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener("click", handleClickOutside);
-    return () => {
-      document.removeEventListener("click", handleClickOutside);
-    };
-  }, []);
+  useOnClickOutside<HTMLElement>([buttonRef, popoverRef], () => {
+    setIsOpen(false);
+  });
 
   return (
     <section>
