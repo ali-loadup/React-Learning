114ae4e652e59411aa540244b62ce00621b105cd
Ali <ali@loadup-talent.de>
2025-04-04 14:05:22 +0200

    using createPortal

--- RemoteDeveloper/src/components/BookmarksPopover.tsx
+++ RemoteDeveloper/src/components/BookmarksPopover.tsx
@@ -1,16 +1,18 @@
 import { forwardRef } from "react";
 import { useBookmarksContext } from "../context/bookmarksContextProvider";
 import JobList from "./JobList";
+import { createPortal } from "react-dom";
 
 export const BookmarksPopover = forwardRef<HTMLDivElement>(function (_, ref) {
   const context = useBookmarksContext();
 
-  return (
+  return createPortal(
     <div className="bookmarks-popover" ref={ref}>
       <JobList
         jobs={context.bookmarkedJobs}
         isLoading={context.isLoading}
       ></JobList>
-    </div>
+    </div>,
+    document.body
   );
 });
