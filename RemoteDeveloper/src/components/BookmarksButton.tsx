c912d5fee723421f1400f07d8be2e271443d5d2a
Ali <ali@loadup-talent.de>
2025-04-04 13:14:48 +0200

    close popover with classNames

--- RemoteDeveloper/src/components/BookmarksButton.tsx
+++ RemoteDeveloper/src/components/BookmarksButton.tsx
@@ -1,9 +1,26 @@
 import { TriangleDownIcon } from "@radix-ui/react-icons";
 import BookmarksPopover from "./BookmarksPopover";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function BookmarksButton() {
   const [isOpen, setIsOpen] = useState(false);
+
+  const handleClickOutside = (event: MouseEvent) => {
+    const target = event.target as HTMLElement;
+    if (
+      !target.closest(".bookmarks-btn") &&
+      !target.closest(".bookmarks-popover")
+    ) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
 
   return (
     <section>
@@ -11,8 +28,7 @@ export default function BookmarksButton() {
         Bookmarks <TriangleDownIcon />
       </button>
 
-{isOpen && <BookmarksPopover /> }
-      
+      {isOpen && <BookmarksPopover />}
     </section>
   );
 }
